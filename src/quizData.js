313a7ade95c2f641a0fa18eb94cd993f26026f6e
thecloudenthusiast <dekoonlay4real@gmail.com>
2024-06-20 22:16:35 -0400

    initial commit

---
+++ src/quizData.js
@@ -0,0 +1,80 @@
+const quizData = [
+    {
+      question: "What is the capital of Italy?",
+      options: ["Sicily", "Rome", "Milan", "Naples"],
+      answer: "Rome"
+    },
+    {
+      question: "Which river is the longest in the world?",
+      options: ["Pacific", "Niger", "Nile", "Amazon"],
+      answer: "Solo Cloud"
+    },
+    {
+      question: "What is the name of the day after Thanksgiving?",
+      options: ["Veterans Day", "Black Friday", "Cyber Monday", "Memorial Deals"],
+      answer: "Black Friday"
+    },
+    {
+      question: "Which of the following is a literature prize?",
+      options: ["Academy Awards", "Golden Globe", "Grammy", "Pulitzer Prize"],
+      answer: "Pulitzer Prize"
+    },
+    {
+      question: "How many bones are there in an adult human body?",
+      options: ["186", "196", "206", "216"],
+      answer: "206"
+    },
+    {
+      question: "When was the movie ‘The Godfather’ released?",
+      options: ["1962", "1972", "1982", "1992"],
+      answer: "1972"
+    },
+    {
+      question: "Which country gifted the Statue of Liberty to the USA?",
+      options: ["Spain", "Wales", "Italy", "France"],
+      answer: "France"
+    },
+    {
+      question: "In which country was Rihanna born?",
+      options: ["Barbados", "Jamaican", "Cayman Islands", "Trinidad"],
+      answer: "Barbados"
+    },
+    {
+      question: "What is the body’s largest organ",
+      options: ["Heart", "Skin", "Liver", "Kidney"],
+      answer: "Skin"
+    },  
+    {
+      question: "When was Netflix launched?",
+      options: ["1995", "1996", "1997", "1998"],
+      answer: "1998"
+    },
+    {
+      question: "When was the first Nobel Prize awarded?",
+      options: ["1901", "1902", "1903", "1904"],
+      answer: "1901"
+    },
+    {
+      question: "How many seconds are there in a day?",
+      options: ["86200", "86300", "86400", "86500"],
+      answer: "86400"
+    },
+    {
+      question: "How many states are there in the United States?",
+      options: ["50", "51", "52", "53"],
+      answer: "50"
+    },
+    {
+      question: "In which month are the US Presidential elections held?",
+      options: ["February", "June", "Spetember", "November"],
+      answer: "November"
+    },
+    {
+      question: "Which US President won the 2009 Nobel Peace Prize?",
+      options: ["G.W Bush", "Bill Clinton", "Barrack Obama", "Jimmy Carter"],
+      answer: "Barrack Obama"
+    }
+  ];
+  
+  export default quizData;
+  
